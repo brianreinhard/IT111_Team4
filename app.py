f622bb1b7bc97e53b7ad2ee7d92dd63b4165a0e3
Shan Huang <shan.huang421@gmail.com>
2026-01-20 20:50:21 -0600

    Add README local run for Windows instructions

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # App metadata
-APP_NAME = "My Flask App"
-APP_VERSION = "1.0"
-APP_DESCRIPTION = "A basic Flask application"
+APP_NAME = "Productivity Web App"
+APP_VERSION = "0.1.0"
+APP_DESCRIPTION = "A basic Flask-based application for organizaing and managing information"
 
 @app.route("/")
 def home():
-    return "Flask is running!"
+    return f"{APP_NAME} is running! (V{APP_VERSION}) - {APP_DESCRIPTION}"
 
 if __name__ == "__main__":
     app.run(debug=True)
